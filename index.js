585dd8025680104e623702d5e2aae157dc5efd72
1512640 <1512640@student.hcmus.edu.vn>
2018-05-05 10:33:20 +0700

    start

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
-var fs = require("fs");
-server.listen(process.env.PORT || 8080);
 
-function player(in_username, in_password, in_status, in_opponent, in_rank, in_exp, in_win, in_lose, in_history){
+server.listen(process.env.PORT || 3000);
+
+function player(in_username, in_password, in_status, in_opponent, in_rank, in_exp, in_win, in_lose, in_history, in_friend){
 	this.username = in_username; // tên đăng nhập
 	this.password = in_password; // mật khẩu
-	this.status = in_status; // trạng thái hiện tại: offline/online/busy
+	this.status = in_status; // trạng thái hiện tại: offline/online/busy (busy là đang bắt trận vs người khác)
 	this.opponent = in_opponent; // đối thủ đang đánh hiện tại: none/tên 1 người chơi khác
 	this.rank = in_rank; // mức rank hiện tại của người chơi
 	this.exp = in_exp; // điểm kinh nghiệm 
 	this.win = in_win; // số trận thắng
 	this.lose = in_lose; // số trận thua
-	this.history = in_history;
+	this.history = in_history; // lịch sử đánh
+	this.friends = in_friend;	// danh sách bạn của người chơi
 }
 
 // Lấy tên đăng nhập
@@ -62,27 +63,39 @@ player.prototype.getHistory = function(){
 	return this.history;
 }
 
+// lấy danh sách bạn
+player.prototype.getFriends = function(){
+	return this.friends;
+}
+
 // Set lịch sử
 player.prototype.setHistory = function(in_history){
 	if(this.history == ""){
-		this.history = in_history
+		this.history = in_history ;
 	}
 	else
 		this.history = this.history + "-" + in_history;
 }
 
+player.prototype.addResultInHistory = function(in_something){
+	this.history = this.history + ":" + in_something;
+}
+
 // Xóa người chơi gần nhất trong lịch sử đấu
 player.prototype.removeOnePlayerInHistoryList = function(){
-	if(this.history == this.opponent){
+	var data = this.history.split("-");
+	if(data.length==1){
 		this.history = "";
-	}else{
-		var data = this.history.split("-");
-		var newHistory = data[0];
-		for(var i = 1; i<data.length-1; i++){
-			newHistory = newHistory + "-" + data[i];
-		}
-		this.history = newHistory;
 	}
+	else{
+		var newHistory = data[0];
+		if(newHistory.length>1){
+			for(var i = 1; i<data.length-1; i++){
+				newHistory = newHistory + "-" + data[i];
+			}
+			this.history = newHistory;
+		}
+	}	
 }
 
 // set 1 đối thủ mới
@@ -113,9 +126,12 @@ player.prototype.minusExp = function(in_exp){
 	this.exp = this.exp - in_exp;
 	// Nếu điểm kinh nghiệm
 	if(this.exp <0 ){
-		this.exp = 9;
-		if(this.rank>1)
-			this.rank = 1;
+		if(this.rank >=2){
+			this.exp = 9;
+			this.rank = this.rank - 1;
+		}else{
+			this.exp = 0;
+		}
 	}
 }
 
@@ -127,6 +143,24 @@ player.prototype.adjustWin = function(){
 // cộng số trận thua
 player.prototype.adjustLose = function(){
 	this.lose = this.lose +1;
+}
+
+// thêm bạn vào danh sách
+player.prototype.addFriend = function(in_friend){
+	if(this.friends == ""){
+		this.friends = in_friend;
+	}
+	else{
+		var x = this.friends.split("-");
+		var exist = false
+		for(var i = 0; i<x.length; i++){
+			if(x[i] == in_friend){
+				exist = true;
+			}
+		}
+		if(exist == false)
+			this.friends = this.friends + "-" + in_friend;
+	}
 }
 
 // Kiểm tra tên đăng nhập
@@ -165,15 +199,12 @@ io.sockets.on('connection', function(socket){
 		if(playerList.length==0){
 			// danh sách rỗng => chưa có tài khoản nào được tạo
 			// thêm mới tài khoản vào danh sách
-			console.log('new account: ' + x[0] + '-' + x[1]);
-			var in_status = "offline";
-			var in_opponent = "none";
-			var in_rank = 1;
-			var in_exp = 0;
-			var in_history = "test";
-			var newPlayer = new player(x[0], x[1], in_status, in_opponent, in_rank, in_exp, 0, 0, "");
+			//console.log('new account: ' + x[0] + '-' + x[1]);
+			var newPlayer = new player(x[0], x[1], "offline", "none", 1, 0, 0, 0, "", "");
 			playerList.push(newPlayer);
-			console.log(newPlayer.getUsername() + "-" + newPlayer.getPassword() + "-" + newPlayer.getStatus());
+			console.log("Tao tai khoan thanh cong: ");
+			console.log("username: "+newPlayer.getUsername());
+			console.log("password: "+newPlayer.getPassword());
 			var success = true;
 			// gởi thông tin về cho người chơi là tạo tài khoản thành công
 			socket.emit('CreateAccoutFeedBack', {feedback:success});
@@ -193,15 +224,17 @@ io.sockets.on('connection', function(socket){
 			if(exist == false){
 				// chưa tồn tại tài khoản
 				// tạo tài khoản mới
-				console.log('new account: ' + x[0] + '-' + x[1]);
+				//console.log('new account: ' + x[0] + '-' + x[1]);
 				var in_status = "offline";
 				var in_opponent = "none";
 				var in_rank = 1;
 				var in_exp = 0;
 				var in_history = "test";
-				var newPlayer = new player(x[0], x[1], in_status, in_opponent, in_rank, in_exp, 0, 0, "");
+				var newPlayer = new player(x[0], x[1], "offline", "none", 1, 0, 0, 0, "", "");
 				playerList.push(newPlayer);
-				console.log(newPlayer.getUsername() + "-" + newPlayer.getPassword() + "-" + newPlayer.getStatus());
+				console.log("Tao tai khoan thanh cong: ");
+				console.log("username: "+newPlayer.getUsername());
+				console.log("password: "+newPlayer.getPassword());
 				var success = true;
 				// gởi thông tin về cho người chơi là tạo tài khoản thành công
 				socket.emit('CreateAccoutFeedBack', {feedback:success});
@@ -223,14 +256,16 @@ io.sockets.on('connection', function(socket){
 			var currentPlayer = playerList[i];
 			var in_username = x[0];
 			var in_password = x[1];
-			if(currentPlayer.CheckAccount(in_username, in_password)){
-				// Khi tìm thấy tài khoản người chơi => người chơi đăng nhập thành công => set trạng thái là online
-				found = true;
-				var in_status = "online";
-				users[currentPlayer.getUsername()] = socket.id;
-				currentPlayer.setStatus(in_status);
-				console.log(currentPlayer.getUsername() + "-" + currentPlayer.getPassword() + "-" + currentPlayer.getStatus() + "-" + users[currentPlayer.getUsername()] + "-" + currentPlayer.getHistory());
-				break;
+			if(playerList[i].getStatus() != "online"){
+				if(currentPlayer.CheckAccount(in_username, in_password)){
+					// Khi tìm thấy tài khoản người chơi => người chơi đăng nhập thành công => set trạng thái là online
+					found = true;
+					var in_status = "online";
+					users[currentPlayer.getUsername()] = socket.id;
+					currentPlayer.setStatus(in_status);
+					console.log("Dang nhap thanh cong voi username la: "+in_username);
+					break;
+				}
 			}
 		}
 		// gởi thông tin về cho người chơi là tìm thấy hay không
@@ -240,48 +275,55 @@ io.sockets.on('connection', function(socket){
 
 	// bắt sự kiện tìm kiếm người chơi ngẫu nhiên
 	socket.on('FindRandomPlayer', function(data){
-		// player1: người chơi đã bấm tìm ngẫu nhiên
-		// player2: người chơi tìm đượcs
-		var found = false;
-		var index = -1;
-		var opponent = "none";
-		var x;
-		var y;
-		for(var i = 0; i<playerList.length; i++){
-			var currentPlayerStatus = playerList[i].getStatus();
-			if(currentPlayerStatus == "online" && currentPlayerStatus != "busy"){
-				var currentPlayerUsername = playerList[i].getUsername();
-				if(currentPlayerUsername != data){
-					// Tìm thấy người chơi phù hợp (player2)
-					// set đối thủ của player2 là player1
-					// set trạng thái lúc này là busy
-					playerList[i].setOpponent(data);
-					playerList[i].setHistory(data);
-					var newStatus = "busy";
-					playerList[i].setStatus(newStatus);
-					y = playerList[i].getOpponent();
-					y = y+"-O-"+playerList[i].getStatus();
-					opponent = currentPlayerUsername;
-					for(var j = 0; j<playerList.length; j++){
-						var currentPlayer = playerList[j];
-						if(currentPlayer.getUsername() == data){
-							// set đối thủ của player1 là player2
-							playerList[j].setOpponent(opponent);
-							playerList[j].setHistory(opponent);
-							playerList[j].setStatus(newStatus);
-							x = playerList[j].getOpponent();
-							x = x+"-X-"+playerList[j].getStatus();
-						}
+			// player1: người chơi đã bấm tìm ngẫu nhiên
+			// player2: người chơi tìm đượcs
+			var opponent = "none";
+			var x;
+			var y;
+			var found = false;
+			do{
+				var i = getRndInteger(0, playerList.length);
+				opponent = "none";
+				x = "";
+				y = "";
+				if(i>=0&&i<=playerList.length){
+					var currentPlayerStatus = playerList[i].getStatus();
+					if(currentPlayerStatus == "online" && currentPlayerStatus != "busy"){
+						var currentPlayerUsername = playerList[i].getUsername();
+						if(currentPlayerUsername != data){
+							// Tìm thấy người chơi phù hợp (player2)
+							// set đối thủ của player2 là player1
+							// set trạng thái lúc này là busy
+							found = true;
+							playerList[i].setOpponent(data);
+							playerList[i].setHistory(data);
+							playerList[i].addResultInHistory("O");
+							var newStatus = "busy";
+							playerList[i].setStatus("busy");
+							y = playerList[i].getOpponent();
+							y = y+"-O-"+playerList[i].getStatus();
+							opponent = currentPlayerUsername;
+							for(var j = 0; j<playerList.length; j++){
+								var currentPlayer = playerList[j];
+								if(currentPlayer.getUsername() == data){
+									// set đối thủ của player1 là player2
+									playerList[j].setOpponent(opponent);
+									playerList[j].setHistory(opponent);
+									playerList[j].addResultInHistory("X");
+									playerList[j].setStatus("busy");
+									x = playerList[j].getOpponent();
+									x = x+"-X-"+playerList[j].getStatus();
+								}
+							}
+						}	
 					}
-					
-					break;
-				}	
-			}
-		}
-		// gởi thông tin về cho player1 username của player2
-		socket.emit('FindRandomPlayerFeedback', {feedback:x});
-		// gởi thông tin về cho player2 username của player1
-		io.to(users[opponent]).emit('FindRandomPlayerFeedback', {feedback:y});
+				}
+			}while(found == false);
+			// gởi thông tin về cho player1 username của player2
+			socket.emit('FindRandomPlayerFeedback', {feedback:x});
+			// gởi thông tin về cho player2 username của player1
+			io.to(users[opponent]).emit('FindRandomPlayerFeedback', {feedback:y});
+		
 	});
 
 
@@ -309,10 +351,10 @@ io.sockets.on('connection', function(socket){
 				opponent = playerList[i].getOpponent();
 				var tmp = "none";
 				var newStatus = "online";
-				playerList[i].setStatus(newStatus);
-				playerList[i].setOpponent(tmp);
+				playerList[i].setStatus("online");
 				playerList[i].removeOnePlayerInHistoryList();
-				x = playerList[i].getOpponent()+"-"+playerList[i].getStatus();
+				playerList[i].setOpponent("none");
+				x = playerList[i].getHistory();
 				break;
 			}
 		}
@@ -321,10 +363,10 @@ io.sockets.on('connection', function(socket){
 			if(currentPlayerUsername == opponent){
 				var tmp = "none";
 				var newStatus = "online";
-				playerList[i].setStatus(newStatus);
-				playerList[i].setOpponent(tmp);
+				playerList[i].setStatus("online");
 				playerList[i].removeOnePlayerInHistoryList();
-				y = playerList[i].getOpponent()+"-"+playerList[i].getStatus();
+				playerList[i].setOpponent("none");
+				y = playerList[i].getHistory();
 			}
 		}
 		socket.emit('CancelFeedback', {feedback:x});
@@ -357,9 +399,9 @@ io.sockets.on('connection', function(socket){
 			if(currentPlayerUsername == data){
 				// set lại đối thủ của người chơi là none, tức là hiện tại không còn đấu với ai
 				var in_opponent = "none";
-				playerList[i].setOpponent(in_opponent);
+				playerList[i].setOpponent("none");
 				var newStatus = "online";
-				playerList[i].setStatus(newStatus);
+				playerList[i].setStatus("online");
 				var x = playerList[i].getOpponent()+"-"+playerList[i].getStatus();
 				// gởi dữ liệu về cho người chơi để người chơi biết là quit thành công
 				socket.emit('QuitFeedback',{feedback:x});
@@ -379,8 +421,13 @@ io.sockets.on('connection', function(socket){
 				var newStatus = "online";
 				var expPoint = getRndInteger(1,2);
 				playerList[i].minusExp(expPoint);
-				playerList[i].setStatus(newStatus);
-				playerList[i].setOpponent(in_opponent);
+				playerList[i].setStatus("online");
+				playerList[i].setOpponent("none");
+				var in_result = "L";
+				playerList[i].addResultInHistory("L");
+				var expPoint1 = getRndInteger(1,2);
+				playerList[i].minusExp(expPoint1);
+				playerList[i].adjustLose();
 				var x = "QuitMiddle: "+playerList[i].getOpponent()+"-"+playerList[i].getStatus()+"-"+playerList[i].getExp();
 				// gởi thông tin cho người chơi để biết là thoát thành công
 				socket.emit('QuitMiddleFeedback',{feedback:x});
@@ -393,8 +440,10 @@ io.sockets.on('connection', function(socket){
 				// Tìm dữ liệu dựa vào username của đối thủ
 				var in_opponent = "none";
 				var newStatus = "online";
-				playerList[i].setStatus(newStatus);
-				playerList[i].setOpponent(in_opponent);
+				playerList[i].setStatus("online");
+				playerList[i].setOpponent("none");
+				var in_result = "W";
+				playerList[i].addResultInHistory("W");
 				var x = playerList[i].getOpponent()+"-"+playerList[i].getStatus();
 				// Gởi thông tin cho đối thủ và cho họ biết là trậu đấu đã kết thúc
 				io.to(users[opponent]).emit('QuitMiddleFeedback', {feedback:x});
@@ -418,6 +467,10 @@ io.sockets.on('connection', function(socket){
 
 	// Bắt sự kiện cập nhật kết quả sau khi trận đấu kết thúc
 	socket.on('Result', function(data){
+		// data sẽ là username của 2 người chơi
+		// vd: player1-player2-win/draw
+		// player1 luôn là player thắng game
+		// player2 là player thua game
 		var opponent = "none";
 		var x = data.split("-");
 		for(var i = 0; i<playerList.length; i++){
@@ -429,20 +482,36 @@ io.sockets.on('connection', function(socket){
 						// người chơi thắng
 						// Cộng số trận thắng của người chơi này
 						// Cộng thêm số điểm kinh nghiệm
-						playerList[i].adjustWin();
 						var expPoint = getRndInteger(2,4);
-						playerList[i].addExp(expPoint);
+						if(x[2]=="win"){
+							playerList[i].adjustWin();
+							playerList[i].addResultInHistory("W");
+							playerList[i].addExp(expPoint);
+						}
+						else{
+							playerList[i].adjustWin();
+							playerList[i].addResultInHistory("D");
+							playerList[i].addExp(1);
+						}
 						var x = playerList[i].getWin();
 						x = x + "-" + playerList[i].getLose() + "-" + playerList[i].getRank() +"-"+playerList[i].getExp();
 						//  người chơi thua
 						// Cộng số trận thua của người chơi này
 						// Trừ điểm kinh nghiệm
 						opponent = playerList[j].getUsername();
-						playerList[j].adjustLose();
 						var expPoint1 = getRndInteger(1,2);
-						playerList[j].minusExp(expPoint1);
+						if(x[2] == "draw"){
+							playerList[j].adjustWin();
+							playerList[j].addExp(1);
+							playerList[j].addResultInHistory("D");
+						}
+						else{
+							playerList[j].adjustLose();
+							playerList[j].addResultInHistory("L");
+							playerList[j].minusExp(expPoint1);
+						}
 						var y = playerList[j].getWin();	
-						y = y + "-" + playerList[j].getLose() + "-" + playerList[j].getRank() +"-"+playerList[j].getExp();;
+						y = y + "-" + playerList[j].getLose() + "-" + playerList[j].getRank() +"-"+playerList[j].getExp();
 						// gởi thông tin về cho người chơi để họ biết là họ đã được cộng thêm 1 trận thắng + kinh nghiệm
 						socket.emit('ResultFeedback', {feedback:x});
 						// gởi thông tin về cho đổi thủ để họ biết là họ đã được cộng thêm 1 trận thua và trừ kinh nghiệm
@@ -455,6 +524,7 @@ io.sockets.on('connection', function(socket){
 
 	// Bắt sự kiện lấy lịch sử đấu của người chơi
 	socket.on('HistoryList', function(data){
+		// data là username của người chơi
 		for(var i = 0; i<playerList.length; i++){
 			var currentPlayerUsername = playerList[i].getUsername();
 			if(currentPlayerUsername == data){
@@ -463,4 +533,128 @@ io.sockets.on('connection', function(socket){
 			}
 		}
 	});
+
+	// Bắt sự kiện thoát khỏi server của người chơi
+	socket.on('GoingOffline', function(data){
+		if(data!=""){
+			var status;
+			for(var i = 0; i<playerList.length; i++){
+				var currentPlayerUsername = playerList[i].getUsername();
+				if(currentPlayerUsername == data){
+					var newStatus = "offline";
+					playerList[i].setStatus("offline");
+					status = playerList[i].getStatus();
+					break;
+				}
+			}
+			//delete users[data];
+			var x = "Nguoi choi: " + data + "- offline";
+			console.log(x);
+			//socket.emit('GoingOfflineFeedback', {feedback:x});
+		}
+	});
+
+	socket.on('GoingOnline', function(data){
+		for(var i = 0; i<playerList.length; i++){
+			if(data == playerList[i].getUsername()){
+				playerList[i].setStatus("online");
+				break;
+			}
+		}
+	});
+
+	// Bắt sự kiện lấy thông tin đối thủ
+	socket.on('OpponentInfo', function(data){
+		for(var i = 0; i<playerList.length; i++){
+			var currentPlayerUsername = playerList[i].getUsername();
+			if(currentPlayerUsername == data){
+				// username-win-lose-rank
+				var x = data+"-"+playerList[i].getWin()+"-"+playerList[i].getLose()+"-"+playerList[i].getRank();
+				socket.emit('OpponentInfoFeedback', {feedback:x});
+				break;
+			}
+		}
+	});
+
+	// bắt sự kiện thêm bạn vào danh sách bạn
+	socket.on('AddFriend', function(data){
+		var x = data.split("-");
+		for(var i = 0; i<playerList.length; i++){
+			var currentPlayerUsername = playerList[i].getUsername();
+			if(currentPlayerUsername == x[0]){
+				playerList[i].addFriend(x[1]);
+				break;
+			}
+		}	
+	});
+
+	// Bắt sự kiện lấy danh sách bạn
+	socket.on('GetFriends', function(data){
+		for(var i = 0; i<playerList.length; i++){
+			var currentPlayerUsername = playerList[i].getUsername();
+			if(currentPlayerUsername == data){
+				var x = playerList[i].getFriends();
+				var status = "";
+				var y = "";
+				var data = x.split("-");
+				for(var j = 0; j<data.length; j++){
+					for(var k = 0; k<playerList.length; k++){
+						if(data[j] == playerList[k].getUsername()){
+							if(y == "")
+								y = playerList[k].getUsername();
+							else
+								y = y + "-" + playerList[k].getUsername();
+
+							y = y + ":" + playerList[k].getStatus(); 
+							break;
+						}	
+					}
+				}
+				socket.emit('GetFriendsFeedback', {feedback:y});
+				break;
+			}
+		}
+	});
+
+	socket.on('ChallengeAFriend', function(data){
+		var x = data.split("-");
+		var y;
+		var z;
+		for(var  i = 0; i<playerList.length; i++){
+			var currentPlayerUsername = playerList[i].getUsername();
+			if(currentPlayerUsername == x[0]){
+				playerList[i].setOpponent(x[1]);
+				playerList[i].setHistory(x[1]);
+				playerList[i].addResultInHistory("X");
+				playerList[i].setStatus("busy");
+				y = playerList[i].getOpponent()+"-X-"+playerList[i].getStatus();
+				for(var j = 0; j<playerList.length; j++){
+					if(playerList[j].getUsername() == x[1]){
+						playerList[j].setOpponent(x[0]);
+						playerList[j].setHistory(x[0]);
+						playerList[j].addResultInHistory("O");
+						playerList[j].setStatus("busy");
+						z = playerList[j].getOpponent()+"-O-"+playerList[j].getStatus();
+						break;
+					}
+				}
+				break;
+			}
+		}
+		socket.emit('ChallengeAFriendFeedback', {feedback:y});
+		io.to(users[x[1]]).emit('ChallengeAFriendFeedback', {feedback:z});
+	});
+
+	socket.on('GetPlayerInfo', function(data){
+		for(var i = 0; i<playerList.length; i++){
+			var currentPlayerUsername = playerList[i].getUsername();
+			if(currentPlayerUsername == data){
+				// username-win-lose-rank
+				var x = data+"-"+playerList[i].getWin()+"-"+playerList[i].getLose()+"-"+playerList[i].getRank()+"-"+playerList[i].getExp();
+				socket.emit('GetPlayerInfoFeedback', {feedback:x});
+				break;
+			}
+		}
+	});
+
 });
